9c6d32517615ef280a55586efff14e5cb57c6870
jjaekong <jjaekong@gmail.com>
2020-05-21 11:10:21 +0900

    테스트 db 설정

--- src/main.js
+++ src/main.js
@@ -13,15 +13,29 @@ Vue.config.productionTip = false
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDUYnKDNULWrHfKPbGxCgUUNKjTysJRAkU",
-    authDomain: "omnitel-coupon-helper.firebaseapp.com",
-    databaseURL: "https://omnitel-coupon-helper.firebaseio.com",
-    projectId: "omnitel-coupon-helper",
-    storageBucket: "omnitel-coupon-helper.appspot.com",
-    messagingSenderId: "900452249197",
-    appId: "1:900452249197:web:6ba3904a5936f540079964"
-};
+let firebaseConfig;
+
+if (process.env.NODE_ENV == 'production') {
+    firebaseConfig = {
+        apiKey: "AIzaSyDUYnKDNULWrHfKPbGxCgUUNKjTysJRAkU",
+        authDomain: "omnitel-coupon-helper.firebaseapp.com",
+        databaseURL: "https://omnitel-coupon-helper.firebaseio.com",
+        projectId: "omnitel-coupon-helper",
+        storageBucket: "omnitel-coupon-helper.appspot.com",
+        messagingSenderId: "900452249197",
+        appId: "1:900452249197:web:6ba3904a5936f540079964"
+    };
+} else {
+    firebaseConfig = {
+        apiKey: "AIzaSyDwALFZn6vNOsKMDAO06J2XSni_U3SELqA",
+        authDomain: "omnitel-coupon-helper-dev.firebaseapp.com",
+        databaseURL: "https://omnitel-coupon-helper-dev.firebaseio.com",
+        projectId: "omnitel-coupon-helper-dev",
+        storageBucket: "omnitel-coupon-helper-dev.appspot.com",
+        messagingSenderId: "983936416628",
+        appId: "1:983936416628:web:2729cc6b813f2ce5fc4a21"
+    };
+}
 
 try {
     firebase.app();
